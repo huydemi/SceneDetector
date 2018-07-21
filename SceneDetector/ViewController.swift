90d72d65b8e2c410cc8a65a08d6c71dc3f62745f
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-21 21:07:46 +0700

    Wrapping the Core ML Model in a Vision Model

--- SceneDetector/ViewController.swift
+++ SceneDetector/ViewController.swift
@@ -29,6 +29,8 @@
  */
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
@@ -59,6 +61,44 @@ extension ViewController {
     pickerController.delegate = self
     pickerController.sourceType = .savedPhotosAlbum
     present(pickerController, animated: true)
+  }
+}
+
+// MARK: - Methods
+extension ViewController {
+  
+  func detectScene(image: CIImage) {
+    answerLabel.text = "detecting scene..."
+    
+    // Load the ML model through its generated class
+    guard let model = try? VNCoreMLModel(for: GoogLeNetPlaces().model) else {
+      fatalError("can't load Places ML model")
+    }
+    
+    // Create a Vision request with completion handler
+    let request = VNCoreMLRequest(model: model) { [weak self] request, error in
+      guard let results = request.results as? [VNClassificationObservation],
+        let topResult = results.first else {
+          fatalError("unexpected result type from VNCoreMLRequest")
+      }
+      
+      // Update UI on main queue
+      let article = (self?.vowels.contains(topResult.identifier.first!))! ? "an" : "a"
+      DispatchQueue.main.async { [weak self] in
+        self?.answerLabel.text = "\(Int(topResult.confidence * 100))% it's \(article) \(topResult.identifier)"
+      }
+    }
+    
+    // Run the Core ML GoogLeNetPlaces classifier on global dispatch queue
+    let handler = VNImageRequestHandler(ciImage: image)
+    DispatchQueue.global(qos: .userInteractive).async {
+      do {
+        try handler.perform([request])
+      } catch {
+        print(error)
+      }
+    }
+    
   }
 }
 
